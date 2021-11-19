1049e16559e55f306853cc4d833c3dc6731cf759
Max Hamilton <maxaham@gmail.com>
2021-11-18 19:08:07 -0700

    Beginning

---
+++ src/components/CartList.vue
@@ -0,0 +1,109 @@
+<template>
+<div class="wrapper">
+  <div class="products">
+    <div class="product" v-for="product in products" :key="product.id">
+      <div class="info">
+        <h1>{{product.name}}</h1>
+        <p>{{product.country}}</p>
+      </div>
+      <div class="image">
+        <img :src="'/images/products/'+product.image">
+      </div>
+      <div class="price">
+        <h3>{{product.price}}</h3>
+        <button class="auto" @click="remove(product)">Remove from Cart</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'CartList',
+  props: {
+    products: Array
+  },
+  methods: {
+    remove(product) {
+        for (var i = this.$root.$data.cart.length - 1; i >= 0; i--) {
+            if (this.$root.$data.cart[i] == product) {
+                this.$root.$data.cart.splice(i, 1);
+            }
+    }
+}
+  },
+}
+
+</script>
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: left;
+  justify-content: left;
+}
+
+.products {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.product {
+  margin: 10px;
+  margin-top: 50px;
+  width: 200px;
+}
+
+.product img {
+  border: 2px solid #333;
+  height: 250px;
+  width: 200px;
+  object-fit: cover;
+}
+
+.product .image {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 5px;
+}
+
+.info {
+  background: #dd5608;
+  color: #000;
+  padding: 10px 30px;
+  height: 80px;
+}
+
+.info h1 {
+  font-size: 16px;
+}
+
+.info h2 {
+  font-size: 14px;
+}
+
+.info p {
+  margin: 0px;
+  font-size: 10px;
+}
+
+
+.price {
+  display: flex;
+  background: black;
+  color: white;
+}
+
+button {
+  height: 60px;
+  background: rgb(236, 164, 9);
+  color: black;
+  border: none;
+}
+
+.auto {
+  margin-left: auto;
+}
+</style>
---
+++ src/views/Cart.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <h1> Your Cart </h1>
+  <CartList :products="cart" />
+</div>
+</template>
+
+<script>
+import CartList from "../components/CartList.vue"
+export default {
+  name: 'Cart',
+  components: {
+    CartList
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    }
+  },
+}
+</script>
